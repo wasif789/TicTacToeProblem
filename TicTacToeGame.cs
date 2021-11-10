a3bd54381cbfe39095a52958aaac237a059e4d80
wasif789 <wsffarid1@gmail.com>
2021-11-10 11:53:00 +0530

    create board

---
+++ TicTacToeGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TicTacToeProblem
+{
+        class TicTacToeGame
+        {
+            public static void BoardInitialise()
+            {
+                char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                foreach (char i in board)
+                {
+                    Console.WriteLine("Element is: {0} ", i);
+                }
+            }
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("welcome to TicTacToe!");
+                BoardInitialise();
+            }
+        }
+}
