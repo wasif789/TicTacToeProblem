e7027c729fb0f0aff66cb1635ac87818cf0fbd40
wasif789 <wsffarid1@gmail.com>
2021-11-10 13:13:21 +0530

    refactor create board

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,12 +6,15 @@ namespace TicTacToeProblem
         {
             public static void BoardInitialise()
             {
-                char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-                foreach (char i in board)
-                {
-                    Console.WriteLine("Element is: {0} ", i);
-                }
+            char[] board = new char[10];
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+                Console.WriteLine("Element is: {0}\t", board[i]);
             }
+
+        }
+
             static void Main(string[] args)
             {
 
